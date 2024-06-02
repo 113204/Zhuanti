f85a9e1b1de32a4bb3d3455394441d1f0b5f8a8f
11236008 <11236008@ntub.edu.tw>
2024-06-02 22:43:31 +0800

    debug

--- media/pose.py
+++ media/pose.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import mediapipe as mp
 import numpy as np
+import winsound
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -31,6 +32,8 @@ begin = []
 start = []
 danger = []
 exercise_time = 0
+freq = 2000
+duration = 1000
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
@@ -81,7 +84,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             right_elbow_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
             right_knee_angle = calculate_angle(right_hip, right_knee, right_ankle)
 
-            if right_elbow_angle > 150:
+            if right_elbow_angle > 150 and right_shoulder_angle > 150:
                 if len(start) == 0:
                     start_time = time.time()
                     start.append(start_time)
@@ -97,20 +100,26 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             if int(started_time) == 1:
                 show_time = 3
+                cv2.putText(img, str(show_time), (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 255, 255), 7,
+                            cv2.LINE_AA)
             elif int(started_time) == 2:
                 show_time = 2
+                cv2.putText(img, str(show_time), (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 255, 255), 7,
+                            cv2.LINE_AA)
             elif int(started_time) == 3:
                 show_time = 1
+                cv2.putText(img, str(show_time), (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 255, 255), 7,
+                            cv2.LINE_AA)
             else:
                 show_time = ''
-            cv2.putText(img, str(show_time), (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 255, 255), 7,
-                        cv2.LINE_AA)
+                cv2.putText(img, str(show_time), (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 6, (255, 255, 255), 7,
+                            cv2.LINE_AA)
 
             if started_time > 3:
-                if right_elbow_angle < 20:
-                    warning_message = 'elbow wrong'
-                    joint_color = (255, 255, 0)
-                    color = (255, 255, 0)
+                if right_elbow_angle < 30 and left_elbow_angle < 30:
+                    warning_message = 'Please hold your right and left hands outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
 
                     if len(danger) == 0:
                         danger_time = time.time()
@@ -121,39 +130,117 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                         die_message = 'die'
                         joint_color = (0, 0, 255)
                         color = (0, 0, 255)
+                        winsound.Beep(freq, duration)
                     elif die_time > 7:
                         die_message = 'maybe die'
-                        joint_color = (0, 255, 255)
-                        color = (0, 255, 255)
+                        joint_color = (0, 133, 242)
+                        color = (0, 133, 242)
 
-                elif 20 < right_elbow_angle < 50 and right_shoulder_angle < 60:
+                elif right_elbow_angle < 30:
+                    warning_message = 'Please hold your right hand outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                    if len(danger) == 0:
+                        danger_time = time.time()
+                        danger.append(danger_time)
+                    die_time = time.time() - danger[0]
+
+                    if die_time > 10:
+                        die_message = 'die'
+                        joint_color = (0, 0, 255)
+                        color = (0, 0, 255)
+                        winsound.Beep(freq, duration)
+                    elif die_time > 7:
+                        die_message = 'maybe die'
+                        joint_color = (0, 133, 242)
+                        color = (0, 133, 242)
+
+                elif left_elbow_angle < 30:
+                    warning_message = 'Please hold your left hand outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                    if len(danger) == 0:
+                        danger_time = time.time()
+                        danger.append(danger_time)
+                    die_time = time.time() - danger[0]
+
+                    if die_time > 10:
+                        die_message = 'die'
+                        joint_color = (0, 0, 255)
+                        color = (0, 0, 255)
+                        winsound.Beep(freq, duration)
+                    elif die_time > 7:
+                        die_message = 'maybe die'
+                        joint_color = (0, 133, 242)
+                        color = (0, 133, 242)
+
+                elif 30 < right_elbow_angle < 50 and right_shoulder_angle < 60 and 30 < left_elbow_angle < 50 and left_shoulder_angle < 60:
                     danger.clear()
                     stage = 'down'
                     warning_message = ''
-                    joint_color = (37, 216, 195)
-                    color = (141, 190, 104)
-                elif 50 < right_elbow_angle < 150 and right_shoulder_angle > 60:
+                    joint_color = (203, 192, 255)
+                    color = (0, 255, 0)
+
+                elif 50 < right_elbow_angle < 150 and right_shoulder_angle > 60 and 50 < left_elbow_angle < 150 and left_shoulder_angle > 60:
                     stage = 'null'
                     warning_message = ''
-                    joint_color = (37, 216, 195)
-                    color = (141, 190, 104)
+                    joint_color = (203, 192, 255)
+                    color = (0, 255, 0)
 
-                if stage == 'down' and (right_shoulder_angle < 30 or right_shoulder_angle > 60):
-                    warning_message = 'shoulder wrong'
-                    joint_color = (255, 255, 0)
-                    color = (255, 255, 0)
+                if stage == 'down' and right_shoulder_angle < 30 and left_shoulder_angle < 30:
+                    warning_message = 'Please put your shoulders outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and right_shoulder_angle < 30:
+                    warning_message = 'Please put your right shoulder outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and left_shoulder_angle < 30:
+                    warning_message = 'Please put your left shoulder outwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and right_shoulder_angle > 60 and left_shoulder_angle > 60:
+                    warning_message = 'Please put your shoulders inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and right_shoulder_angle > 60:
+                    warning_message = 'Please put your right shoulder inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and left_shoulder_angle > 60:
+                    warning_message = 'Please put your left shoulder inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                elif stage == 'down' and right_elbow_angle > 50 and left_elbow_angle > 50:
+                    warning_message = 'Please hold your left and right hands inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
                 elif stage == 'down' and right_elbow_angle > 50:
-                    warning_message = 'elbow wrong'
-                    joint_color = (255, 255, 0)
-                    color = (255, 255, 0)
+                    warning_message = 'Please hold your right hand inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
 
-                if right_elbow_angle > 150 and stage == 'null':
+                elif stage == 'down' and left_elbow_angle > 50:
+                    warning_message = 'Please hold your left hand inwards'
+                    joint_color = (0, 255, 255)
+                    color = (0, 255, 255)
+
+                if right_elbow_angle > 150 and left_elbow_angle > 150 and stage == 'null':
                     stage = 'up'
                     warning_message = ''
                     counter += 1
-                    joint_color = (37, 216, 195)
-                    color = (141, 190, 104)
-                    print(counter)
+                    joint_color = (203, 192, 255)
+                    color = (0, 255, 0)
+
                 print(stage)
                 mp_drawing.draw_landmarks(
                     img,
@@ -165,12 +252,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 )
 
                 cv2.rectangle(img, (0, 0), (140, 40), (255, 0, 0), -1)
-                cv2.rectangle(img, (200, 0), (450, 40), (0, 255, 255), -1)
-                cv2.rectangle(img, (550, 0), (650, 40), (0, 0, 255), -1)
+                cv2.rectangle(img, (0, 40), (650, 80), (0, 255, 255), -1)
+                cv2.rectangle(img, (585, 0), (650, 40), (0, 0, 255), -1)
                 cv2.putText(img, str(exercise_time), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
                             cv2.LINE_AA)
-                cv2.putText(img, str(warning_message), (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
-                cv2.putText(img, str(counter), (580, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.putText(img, str(warning_message), (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(img, str(counter), (590, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
             else:
                 warning_message = ''
@@ -182,17 +269,18 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                     img,
                     results.pose_landmarks,
                     mp_pose.POSE_CONNECTIONS,
-                    mp_drawing.DrawingSpec(color=(37, 216, 195), thickness=5, circle_radius=4),
-                    mp_drawing.DrawingSpec(color=(141, 190, 104), thickness=2, circle_radius=2),
+                    mp_drawing.DrawingSpec(color=(203, 192, 255), thickness=5, circle_radius=4),
+                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
                     # landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
                 )
 
                 cv2.rectangle(img, (0, 0), (140, 40), (255, 0, 0), -1)
-                cv2.rectangle(img, (200, 0), (450, 40), (0, 255, 255), -1)
-                cv2.rectangle(img, (550, 0), (650, 40), (0, 0, 255), -1)
-                cv2.putText(img, str(exercise_time), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                cv2.putText(img, str(warning_message), (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
-                cv2.putText(img, str(counter), (580, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+                cv2.rectangle(img, (0, 40), (650, 80), (0, 255, 255), -1)
+                cv2.rectangle(img, (585, 0), (650, 40), (0, 0, 255), -1)
+                cv2.putText(img, str(exercise_time), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                            cv2.LINE_AA)
+                cv2.putText(img, str(warning_message), (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(img, str(counter), (590, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
             cv2.putText(img, str(int(left_elbow_angle)), tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
                         cv2.FONT_ITALIC, 1, (0, 0, 255), 2)
@@ -212,7 +300,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             pass
 
         img = cv2.resize(img, (960, 720))
-        cv2.imshow('usb_camera', img)
+        cv2.imshow('pose', img)
         if cv2.waitKey(1) == ord('q'):
             break
 
